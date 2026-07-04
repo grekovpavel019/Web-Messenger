c3a1b2ce42357723acbac26bbeb862cb2d963acb
grekovpavel019 <grekovpavel065@gmail.com>
2026-07-04 19:21:26 +0300

    minor changes

---
+++ server/scema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    login TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
--- server/server.js
+++ server/server.js
@@ -1,20 +1,50 @@
 import express from "express";
 import path from "path";
 
+import session from "express-session";
+
+import { Pool } from "pg";
+
 const app = express();
 
 const publicPath = path.resolve("../public");
 
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "messenger",
+    password: "admin",
+    port: 5432
+});
+
+const result = await pool.query("SELECT * FROM users");
+console.log(result.rows);
+
+app.use(session({
+    secret: "super-secret-key",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}));
 app.use(express.static(publicPath));
 app.use(express.json());
 
-const users = new Map();
-console.log(users);
-
-app.post("/api/register", (req, res) => {
+app.post("/api/register", async (req, res) => {
+    if (req.session.user) {
+        return res.status(403).json({
+            message: "Ты уже вошел"
+        })
+    }
     const { login, email, password } = req.body;
+
+    const existingUser = await pool.query(
+        "SELECT * FROM users WHERE login = $1", [login]
+    );
     
-    if (users.has(login)) {
+    if (existingUser.rows.length > 0) {
         return res.status(409).json({
             message: "Такой пользователь уже есть"
         })
@@ -26,9 +56,15 @@ app.post("/api/register", (req, res) => {
         password
     }
 
-    users.set(user.login, user);
+    await pool.query(
+        `
+        INSERT INTO users (login, email, password)
+        VALUES ($1, $2, $3)
+        `,
+        [login, email, password]
+    );
 
-    res.status(200).json({
+    res.status(201).json({
         message: "Пользователь создан",
         user: {
             login: user.login
@@ -38,21 +74,30 @@ app.post("/api/register", (req, res) => {
     console.log(users);
 });
 
-app.post("/api/login", (req, res) => {
+app.post("/api/login", async (req, res) => {
     const { login, password } = req.body;
 
-    if (!users.has(login)) {
+    const result = await pool.query(
+        'SELECT * FROM users WHERE login = $1',
+        [login]
+    );
+
+    if (result.rows.length === 0) {
         return res.status(401).json({
             message: "Такого пользователя не существует"
         });
     }
 
-    const user = users.get(login);
+    const user = result.rows[0];
 
-    if (!(password === user.password)) {
+    if (password !== user.password) {
         return res.status(403).json({
             message: "Неправильный пароль"
         });
+    }
+
+    req.session.user = {
+        login: user.login
     }
 
     res.status(200).json({
@@ -62,8 +107,15 @@ app.post("/api/login", (req, res) => {
         }
     });
 
-    console.log(users);
 });
+
+app.get("/chat.html", (req, res) => {
+    if (!req.session.user) {
+        return res.redirect("/login.html");
+    }
+
+    res.sendFile(path.join(publicPath, "chat.html"));
+})
 
 app.listen(3000, () => {
     console.log("Сервер работает");
