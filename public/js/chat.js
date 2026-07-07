1780962232295c2be9640b99aff4f6d861651b29
grekovpavel019 <grekovpavel065@gmail.com>
2026-07-07 19:55:22 +0300

    обновлен вебсокет

--- public/js/chat.js
+++ public/js/chat.js
@@ -21,7 +21,7 @@ async function init() {
 init();
 
 function connectWS() {
-    ws = new WebSocket("ws://localhost:3000");
+    ws = new WebSocket(`ws://${location.host}`);
 
     ws.onopen = () => {
         ws.send(JSON.stringify({
